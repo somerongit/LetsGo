06917fac8570420802f2a8ef443a459ab5cce626
somerongit <someron.bakuli05@gmail.com>
2022-07-06 09:36:16 +0530

    GCD LCM

--- hello.go
+++ hello.go
@@ -2,35 +2,35 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	var size int
-	print("Enter input size: ")
-	fmt.Scan(&size)
+	var num, num1 int64
+	print("Enter 1st num: ")
+	fmt.Scan(&num)
 
-	set := make([]float64, size)
+	print("Enter 2nd num: ")
+	fmt.Scan(&num1)
 
-	println("Enter the values: ")
-	for i := 0; i < size; i++ {
-		var temp float64
-		fmt.Scan(&temp)
-		set[i] = temp
-	}
-	println("Input values are")
-	for i := 0; i < size; i++ {
-		println(i, "-->", int(set[i]))
-	}
+	println("Input values are: ", num, ",", num1)
 
-	print("Cardinality of input value=> ")
-	println(getCardinality(set))
+	print("\nGCD of input values=> ")
+	start := time.Now()
+	println(getHcfBruteForce(num, num1))
+	end := time.Since(start)
+	println("Time taken(BruteForce): ", end)
 
-	print("Mean of input value=> ")
-	println(int64(getMean(set)))
+	print("\nGCD of input values=> ")
+	start1 := time.Now()
+	println(getHcfNew(num, num1))
+	end1 := time.Since(start1)
+	println("Time taken(New): ", end1)
 
-	print("Variance of input value=> ")
-	println(int64(getVariance(set)))
+	print("\nGCD of input values=> ")
+	start2 := time.Now()
+	println(getHcfRecursion(num, num1))
+	end2 := time.Since(start2)
+	println("Time taken(Recursion): ", end2)
 
-	print("Standard Deviation of input value=> ")
-	println(int64(getStandardDeviation(set)))
 }
